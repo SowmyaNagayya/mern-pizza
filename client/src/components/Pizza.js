7c1f2f17a01ee59ab962eedfcc46c84a7c9b742b
sowmyanagayya <kkd.sowmya@gmail.com>
2022-03-09 21:07:56 -0600

    worked on Pizza component to render data on page, rendered name and variants and quantity still working on Pizza component

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -3,7 +3,30 @@ import React from 'react'
 function Pizza({pizza}) {
   return (
     <div>
-        d
+        <h1>{pizza.name}</h1>
+        <img src={pizza.image} className="img-fluid" style={{height:'200px', width: '200px'}}/>
+
+        <div className="flex-container">
+            <div className="w-100">
+                <p>Variants</p>
+                <select>
+                {pizza.variants.map(variant =>{
+                    return <option value={variant}>{variant}</option>
+                })}
+                </select>
+            </div>
+
+            <div className="w-100">
+                <p>Quantity</p>
+                <select>
+                    {/* Array gives number 1 to 10 and keys are nothing but 1 to 10 , for empty array we use 2 parameters 1st parameter i.e "x and index "i"*/}
+                    {[...Array(10).keys()].map((x, i) => {
+                        // i+1 because index starts with 0 so, 0+1
+                        return <option value={i+1}>{i+1}</option>
+                    })} 
+                </select>
+            </div>
+        </div>
     </div>
   )
 }
