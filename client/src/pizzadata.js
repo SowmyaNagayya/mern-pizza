4f96ce20743cbc9aad60bb1d5fd165a9747dcd62
sowmyanagayya <kkd.sowmya@gmail.com>
2022-03-09 20:04:25 -0600

    added dummy data file

---
+++ client/src/pizzadata.js
@@ -0,0 +1,109 @@
+[{
+    "name": "PEPPER BARBECUE CHICKEN",
+    "variants": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "nonveg",
+    "image": "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    "description": "Pepper Barbeque Chicken I Cheese"
+},
+{
+    "name": "Non Veg Supreme",
+    "variants": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "nonveg",
+    "image": "https://www.dominos.co.in/files/items/Non-Veg_Supreme.jpg",
+    "description": "Bite into supreme of Black olives , Onions, Grilled Mushrooms, Pepper BBQ Chicken, Peri-Peri"
+},
+{
+    "name": "Golden Corn Pizza",
+    "variants": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 180,
+            "medium": 250,
+            "large": 400
+        }
+    ],
+    "category": "nonveg",
+    "image": "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    "description": "Pepper Barbeque Chicken I Cheese"
+},
+{
+    "name": "PEPPER BARBECUE CHICKEN",
+    "variants": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "nonveg",
+    "image": "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    "description": "Pepper Barbeque Chicken I Cheese"
+},
+{
+    "name": "PEPPER BARBECUE CHICKEN",
+    "variants": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "nonveg",
+    "image": "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    "description": "Pepper Barbeque Chicken I Cheese"
+},
+{
+    "name": "PEPPER BARBECUE CHICKEN",
+    "variants": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "nonveg",
+    "image": "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    "description": "Pepper Barbeque Chicken I Cheese"
+}
+]
