811cb3871b52bff03a8eb0169f3011acb3cc58c0
sowmyanagayya <kkd.sowmya@gmail.com>
2022-03-11 11:50:15 -0600

    using axios get the data from backend and populated on frontend and removed static data

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -9,7 +9,7 @@ function Pizza({ pizza }) {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     return (
-        <div style={{ margin: '50px' }} className="shadow-lg p-3 mb-5 bg-white rounded">
+        <div className="shadow-lg p-3 mb-5 bg-white rounded">
             <div onClick={handleShow}>
                 <h1>{pizza.name}</h1>
                 <img src={pizza.image} className="img-fluid" style={{ height: '200px', width: '200px' }} />
