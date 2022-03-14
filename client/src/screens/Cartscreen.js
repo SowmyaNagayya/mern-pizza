2285b26d2b9b84827bbc54515797d08486dbd0cb
sowmyanagayya <kkd.sowmya@gmail.com>
2022-03-14 15:32:38 -0500

    worked on 1st div display cart items with details, now working on display image and subtotal

--- client/src/screens/Cartscreen.js
+++ client/src/screens/Cartscreen.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 function Cartscreen() {
+    const cartstate = useSelector(state=>state.cartReducer)
+    const cartItems = cartstate.cartItems
   return (
     <div>
-        <h1>Cart Screen</h1>
+        <div className="row justify-content-center">
+            <div className="col-md-6">
+                <h2 style={{fontSize:"40px", fontWeight:"bold"}}>My Cart</h2>
+                {cartItems.map(item=> {
+                    return <div className="flex-container">
+                    <div style={{textAlign:"left"}} className="m-1">
+                        <h1>{item.name} [{item.variant}]</h1>
+                        <h1>Price: {item.quantity} * {item.prices[0][item.variant]} = {item.price}</h1>
+                        <h1 style={{display:'inline'}}>Quantity: </h1>
+                        <i className="fa fa-plus" aria-hidden="true"></i>
+                        <b>{item.quantity}</b>
+                        <i className="fa fa-minus" aria-hidden="true"></i>
+                        <hr/>
+                    </div>
+                    <div>
+                        
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+                })}
+                
+            </div>
+
+            <div className="col-md-4">
+                d
+            </div>
+        </div>
     </div>
   )
 }
