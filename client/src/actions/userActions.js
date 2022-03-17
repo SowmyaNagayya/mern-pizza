8e5393bc80ee8c115fc69c75d775e6707d7977cf
sowmyanagayya <kkd.sowmya@gmail.com>
2022-03-17 13:25:13 -0500

    worked on register reducer and action and store

---
+++ client/src/actions/userActions.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+export const registerUser = () =>async dispatch=> {
+    dispatch({type: 'USER_REGISTER_REQUEST'})
+    try {
+        const response = await axios.post('/api/users/register', {user})
+        console.log(response)
+        dispatch({type: 'USER_REGISTER_SUCCESS'})
+    } catch {
+        dispatch({type: 'USER_REGISTER_FAILED', payload: error})      
+    }
+}
