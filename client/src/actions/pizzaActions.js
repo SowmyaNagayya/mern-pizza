98d7cc513f62fc4cfadd475bdfd4a0a5323082af
sowmyanagayya <kkd.sowmya@gmail.com>
2022-03-21 14:40:52 -0500

    worked on filtering pizza by giving input values in search pizza

--- client/src/actions/pizzaActions.js
+++ client/src/actions/pizzaActions.js
@@ -12,3 +12,21 @@ export const getAllPizzas=()=>async dispatch=> {
         dispatch({type: 'GET_PIZZAS_FAILED', payload: error})
     }
 }
+
+//filter 
+export const filterPizzas=(searchkey, category)=>async dispatch=> {
+    var filteredPizzas;
+    dispatch({type: 'GET_PIZZAS_REQUEST'})
+
+    try {
+        const response = await axios.get('/api/pizzas/getallpizzas');
+        console.log(response)
+        filteredPizzas = response.data.filter(pizza=> pizza.name.toLowerCase().includes(searchkey))
+        if(category!='all') {
+            filteredPizzas = response.data.filter(pizza=> pizza.category.toLowerCase()===category)
+        }
+        dispatch({type: 'GET_PIZZAS_SUCCESS', payload: filteredPizzas})
+    } catch (error) {
+        dispatch({type: 'GET_PIZZAS_FAILED', payload: error})
+    }
+}
--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import {filterPizzas} from "../actions/pizzaActions"
 
 export default function Filter() {
     const dispatch = useDispatch()
+    const [searchkey, setSearchkey] = useState("");
+    const [category, setCategory] = useState("all")
   return (
     <div className="container">
         <div className="row justify-content-center shadow-lg p-3 mb-5 bg-white rounded">
             <div className="col-md-3">
-                <input type="text" className="form-control" placeholder="Search Pizza" />
+                <input type="text" value={searchkey} onChange={(e)=>setSearchkey(e.target.value)}className="form-control" placeholder="Search Pizza" />
             </div>
 
             <div className="col-md-3 mt-3">
-                <select className="form-control">
+                <select className="form-control" value={category} onChange={(e)=>setCategory(e.target.value)}>
                     <option value="all">All</option>
                     <option value="veg">Veg</option>
                     <option value="nonveg">Non-Veg</option>
@@ -19,7 +22,7 @@ export default function Filter() {
             </div>
 
             <div className="col-md-3 mt-3">
-                <button className="btn">Filter</button>
+                <button className="btn" onClick={()=>dispatch(filterPizzas(searchkey, category))}>Filter</button>
             </div>
         </div>
     </div>
