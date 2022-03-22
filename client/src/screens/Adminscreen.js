746d5b010531ae2dacd533ee98ef3a679818f80c
sowmyanagayya <kkd.sowmya@gmail.com>
2022-03-22 09:43:17 -0500

    worked on adminpanel nav bar and some styling

--- client/src/screens/Adminscreen.js
+++ client/src/screens/Adminscreen.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 
 export default function Adminscreen() {
-  return (
-    <div>
-        <h1 style={{fontSize:'35px'}}>Admin Panel</h1>
-    </div>
-  )
+    const userstate = useSelector(state => state.loginUserReducer)
+    const { currentUser } = userstate;
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (currentUser.isAdmin) {
+            window.location.href = '/'
+        }
+    }, [])
+    return (
+        <div>
+            <div className="row justify-content-center">
+                <div className="col-md-10">
+                    <h1 style={{ fontSize: '35px' }}>Admin Panel</h1>
+                    <ul className="adminfunctions">
+                        <li><a href="">Users List</a></li>
+                        <li><a href="">Pizzas List</a></li>
+                        <li><a href="">Add New Pizza</a></li>
+                        <li><a href="">Orders List</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
